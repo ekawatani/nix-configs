d175bdc9aaec67f6f2d98d9784ab36ea8ce9a43b
Eigo Kawatani <e.k.net@outlook.com>
2025-10-18 00:27:18 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.core = nixpkgs.lib.nixosSystem {
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix"
+        ./configuration.nix
+      ];
+    };
+
+  };
+}
